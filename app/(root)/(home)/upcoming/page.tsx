299a7e2be20e468da5730471a50d24edcb90d7f0
AshrithSathu <sathuashrith@gmail.com>
2024-04-22 09:56:47 +0530

    schedule,upcoming,previous done

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,11 @@
+import CallList from "@/components/CallList";
 import React from "react";
 
 function Upcoming() {
   return (
     <section className="flex size-full flex-col text-white">
       <h1 className="text-3xl font-bold">Upcoming</h1>
+      <CallList type="upcoming" />
     </section>
   );
 }
